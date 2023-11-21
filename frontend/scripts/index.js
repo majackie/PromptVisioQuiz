6f42136d23b1ba323ebcd0835539bcac3922646d
Jackie Ma <97272592+majackie@users.noreply.github.com>
2023-11-21 15:17:00 -0800

    add redirect if user is or not logged in

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,39 +1,43 @@
 // ChatGPT-3.5 (https://chat.openai.com/) was used to code solutions presented in this assignment
 
+if (localStorage.getItem('isLoggedIn') === 'true') {
+    window.location.href = 'model.html';
+}
+
 const url = 'http://localhost:3000';
 
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  
-  fetch(url + '/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ username, password }),
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    localStorage.setItem('isLoggedIn', 'true');
-    window.location.href = 'model.html';
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    fetch(url + '/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            localStorage.setItem('isLoggedIn', 'true');
+            window.location.href = 'model.html';
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 });
 
-document.getElementById('logoutButton').addEventListener('click', function() {
-  // Remove 'isLoggedIn' from localStorage
-  localStorage.removeItem('isLoggedIn');
-  
-  // Redirect to the login page
-  window.location.href = 'index.html';
+document.getElementById('logoutButton').addEventListener('click', function () {
+    // Remove 'isLoggedIn' from localStorage
+    localStorage.removeItem('isLoggedIn');
+
+    // Redirect to the login page
+    window.location.href = 'index.html';
 });
--- frontend/scripts/model.js
+++ frontend/scripts/model.js
@@ -1,82 +1,82 @@
 // ChatGPT-3.5 (https://chat.openai.com/) was used to code solutions presented in this assignment
 
 if (localStorage.getItem('isLoggedIn') !== 'true') {
-  window.location.href = 'index.html';
+    window.location.href = 'index.html';
 }
 
 const url = 'http://localhost:3000';
 
 document.getElementById('generateButton').addEventListener('click', function () {
-  fetch(url + '/model')
-  .then(response => response.text())
-  .then(message => {
-    console.log(message);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+    fetch(url + '/model')
+        .then(response => response.text())
+        .then(message => {
+            console.log(message);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 });
 
 document.getElementById('questionaireButton').addEventListener('click', function () {
-  fetch(url + '/titles')
-    .then(response => response.json())
-    .then(data => {
-      const br = document.createElement('br');
-      const dataContainer = document.getElementById('dataContainer');
-      dataContainer.innerHTML = '';
+    fetch(url + '/titles')
+        .then(response => response.json())
+        .then(data => {
+            const br = document.createElement('br');
+            const dataContainer = document.getElementById('dataContainer');
+            dataContainer.innerHTML = '';
 
-      const img = document.createElement('img');
-      img.src = url + '/image';
-      dataContainer.appendChild(img);
-      dataContainer.appendChild(br);
+            const img = document.createElement('img');
+            img.src = url + '/image';
+            dataContainer.appendChild(img);
+            dataContainer.appendChild(br);
 
-      const solutionTitle = data.titles[0];
+            const solutionTitle = data.titles[0];
 
-      data.titles.sort(() => Math.random() - 0.5);
-      data.titles.forEach((item, index) => {
-        const number = index + 1;
+            data.titles.sort(() => Math.random() - 0.5);
+            data.titles.forEach((item, index) => {
+                const number = index + 1;
 
-        const radio = document.createElement('input');
-        radio.type = 'radio';
-        radio.name = 'title';
-        radio.value = item;
-        dataContainer.appendChild(radio);
+                const radio = document.createElement('input');
+                radio.type = 'radio';
+                radio.name = 'title';
+                radio.value = item;
+                dataContainer.appendChild(radio);
 
-        const label = document.createElement('label');
-        label.textContent = `${number}) ${item}`;
-        dataContainer.appendChild(label);
+                const label = document.createElement('label');
+                label.textContent = `${number}) ${item}`;
+                dataContainer.appendChild(label);
 
-        dataContainer.appendChild(br.cloneNode());
-      });
+                dataContainer.appendChild(br.cloneNode());
+            });
 
-      const button = document.createElement('button');
-      button.textContent = 'Submit';
-      dataContainer.appendChild(button);
+            const button = document.createElement('button');
+            button.textContent = 'Submit';
+            dataContainer.appendChild(button);
 
-      const result = document.createElement('h2');
-      result.id = 'resultMessage';
-      dataContainer.appendChild(result);
+            const result = document.createElement('h2');
+            result.id = 'resultMessage';
+            dataContainer.appendChild(result);
 
-      button.addEventListener('click', function () {
-        const selectedRadio = document.querySelector('input[name="title"]:checked');
-        if (selectedRadio) {
-          const selectedTitle = selectedRadio.value;
-          const resultMessage = document.getElementById('resultMessage');
-          if (selectedTitle === solutionTitle) {
-            resultMessage.textContent = 'Correct!';
-          } else {
-            resultMessage.textContent = 'Incorrect!';
-          }
-        } else {
-          const resultMessage = document.getElementById('resultMessage');
-          resultMessage.textContent = 'Please select a title.';
-        }
-      });
-    })
-    .catch(error => console.error('Error:', error));
+            button.addEventListener('click', function () {
+                const selectedRadio = document.querySelector('input[name="title"]:checked');
+                if (selectedRadio) {
+                    const selectedTitle = selectedRadio.value;
+                    const resultMessage = document.getElementById('resultMessage');
+                    if (selectedTitle === solutionTitle) {
+                        resultMessage.textContent = 'Correct!';
+                    } else {
+                        resultMessage.textContent = 'Incorrect!';
+                    }
+                } else {
+                    const resultMessage = document.getElementById('resultMessage');
+                    resultMessage.textContent = 'Please select a title.';
+                }
+            });
+        })
+        .catch(error => console.error('Error:', error));
 });
 
-document.getElementById('logoutButton').addEventListener('click', function() {
-  localStorage.removeItem('isLoggedIn');
-  window.location.href = 'index.html';
+document.getElementById('logoutButton').addEventListener('click', function () {
+    localStorage.removeItem('isLoggedIn');
+    window.location.href = 'index.html';
 });
