b7638d48135b62292c4341c380ffbd6343e093af
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-11-27 19:12:21 -0800

    admin jwt token

--- frontend/scripts/admin.js
+++ frontend/scripts/admin.js
@@ -1,17 +1,32 @@
-// ChatGPT-3.5 (https://chat.openai.com/) was used to code solutions presented in this assignment
+// Check if a valid JWT token is present and the role is 'admin'
+const token = localStorage.getItem('token');
 
-// Check if the user is logged in and if the role is 'admin'
-if (localStorage.getItem('isLoggedIn') !== 'true' || localStorage.getItem('role') !== 'admin') {
+if (!token) {
     // If not, redirect to the login page
     window.location.href = 'index.html';
+} else {
+    try {
+        // Decode the token to get its payload
+        const decodedToken = JSON.parse(atob(token.split('.')[1]));
+
+        // Check if the role is 'admin'
+        if (decodedToken.role !== 'admin') {
+            // If not, redirect to the login page
+            window.location.href = 'index.html';
+        }
+    } catch (error) {
+        // Handle invalid or expired token
+        console.error('Error decoding or validating token:', error);
+        // Redirect to the login page
+        window.location.href = 'index.html';
+    }
 }
 
-window.onload = function() {
-    // Get the button
+// Set the button click event
+document.addEventListener('DOMContentLoaded', function () {
     var button = document.querySelector('.container button');
 
-    // Set the button click event
     button.onclick = function() {
         window.location.href = 'questionaire.html';
     };
-};
+});
