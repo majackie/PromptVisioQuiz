db9b844dc5124e18bfa995f497f3a5ff08db99f3
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-12-03 04:15:41 -0800

    updated origins

--- backend/app.js
+++ backend/app.js
@@ -29,9 +29,18 @@ const pool = new pg.Pool({
 // Middleware for CORS and JSON parsing
 app.use(cors({
     origin: 'https://promptvisioquizfrontend.onrender.com',
-    // origin: '*',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://promptvisioquizfrontend.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
