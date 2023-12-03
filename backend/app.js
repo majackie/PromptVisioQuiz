f14fb6302dbf8b2527638998e7505adcb20f6662
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-12-02 22:51:52 -0800

    correct cors

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ let isModelRunning = false;
 
 // Middleware for CORS and JSON parsing
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
+    origin: 'https://promptvisioquizfrontend.onrender.com',
     credentials: true,
 }));
 app.use(express.json());
