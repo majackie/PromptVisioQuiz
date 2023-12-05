a4313ae58842cdcec2bf4fc65660526ab68a33d2
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-12-04 22:40:17 -0800

    udpated cors

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', url);
+    res.header('Access-Control-Allow-Origin', 'https://promptvisioquizfrontend.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
