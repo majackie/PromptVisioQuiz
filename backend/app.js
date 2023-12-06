7bdd42c41f208cb10b39f736060d2e020ceffa36
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2023-12-05 23:34:26 -0800

    deleted unnecessary pools

--- backend/app.js
+++ backend/app.js
@@ -169,7 +169,6 @@ app.delete('/admin/user_accounts/:userId', verifyToken, async (req, res) => {
 app.get('/apiCount', verifyToken, async (req, res) => {
     try {
         // Get the api_count value from the database
-        const client = await pool.connect();
 
         // Using template string for the SQL query
         const sql = `
@@ -180,7 +179,7 @@ app.get('/apiCount', verifyToken, async (req, res) => {
         `;
 
         // Execute the query
-        const result = await client.query(sql, [req.user.username]);
+        const result = pool.query(sql, [req.user.username]);
 
         // Check if any rows were returned
         if (result.rows.length > 0) {
@@ -191,7 +190,6 @@ app.get('/apiCount', verifyToken, async (req, res) => {
             // Handle the case when no rows are returned (username not found)
             res.status(404).send({ error: 'Username not found' });
         }
-        client.release();
     } catch (error) {
         console.error('Error in /apiCount route:', error);
         res.status(500).send('Internal Server Error');
@@ -370,7 +368,6 @@ app.post('/login', async (req, res) => {
         }
     
     });
-    pool.release();
 });
 
 app.get('/logout', (req, res) => {
